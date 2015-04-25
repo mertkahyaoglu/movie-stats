b1ad593a141456884d4c8a077d45bc6313b7c028
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2015-04-25 17:22:08 +0300

    remove unnecessary imports

--- webmining/spiders/movies.py
+++ webmining/spiders/movies.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 from scrapy import Request
 from .. import items
@@ -9,9 +7,7 @@ from .. import items
 class MoviesSpider(scrapy.Spider):
     name = "movies"
     allowed_domains = ["imdb.com"]
-    start_urls = (
-        'http://www.imdb.com/chart/top',
-    )
+    start_urls = ('http://www.imdb.com/chart/top')
 
     def parse(self, response):
         movie_links = response.css("tbody.lister-list tr td.titleColumn a::attr(href)").extract()
