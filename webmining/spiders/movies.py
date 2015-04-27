4da66623b1402f8f0c9e34796730e862c64fabde
Mert Kahyaoğlu <mertkahyaoglu93@gmail.com>
2015-04-28 01:42:21 +0300

    fix error

--- webmining/spiders/movies.py
+++ webmining/spiders/movies.py
@@ -7,14 +7,15 @@ from .. import items
 class MoviesSpider(scrapy.Spider):
     name = "movies"
     allowed_domains = ["imdb.com"]
-    start_urls = ('http://www.imdb.com/chart/top')
+    start_urls = (
+        'http://www.imdb.com/chart/top',
+    )
 
     def parse(self, response):
         movie_links = response.css("tbody.lister-list tr td.titleColumn a::attr(href)").extract()
         base_comment_url = "http://www.imdb.com/%s"
         for link in movie_links:
             yield Request(base_comment_url % link, self.parse_movie)
-
     def parse_movie(self, response):
         item = items.MovieItem()
         url = response.url
@@ -23,7 +24,7 @@ class MoviesSpider(scrapy.Spider):
         item['year'] = response.css("td#overview-top h1.header span.nobr a::text").extract()[0]
         item['rating'] = response.css("div.star-box-giga-star::text").extract()[0].strip()
         item['director'] = response.css("div[itemprop=director] a span::text").extract()[0]
-        item['place'] = response.css("div#titleAwardsRanks a strong::text").extract()[0].split('#')[1]
+        item['place'] = int(response.css("div#titleAwardsRanks a strong::text").extract()[0].split('#')[1])
         item['genres'] = response.css("div.infobar span[itemprop=genre]::text").extract()
         item['cast'] = response.css("table.cast_list tr td[itemprop=actor] a span::text").extract()
         item['poster'] = response.css("div.image img[itemprop=image]::attr(src)").extract()[0]
